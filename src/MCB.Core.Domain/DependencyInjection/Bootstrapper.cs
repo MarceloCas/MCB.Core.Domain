23b75c20e6949d307b3cca27afc7e8066f4bedfb
Marcelo Castelo Branco <marcelo.castelo@outlook.com>
2022-11-07 01:22:19 -0300

    first commit

---
+++ src/MCB.Core.Domain/DependencyInjection/Bootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using MCB.Core.Domain.Abstractions.DomainEvents;
+using MCB.Core.Domain.DomainEvents;
+using MCB.Core.Domain.DomainEvents.Interfaces;
+using MCB.Core.Infra.CrossCutting.DependencyInjection.Abstractions.Interfaces;
+
+namespace MCB.Core.Domain.DependencyInjection;
+
+public static class Bootstrapper
+{
+    public static void ConfigureServices(IDependencyInjectionContainer dependencyInjectionContainer)
+    {
+        // Domain Events
+        dependencyInjectionContainer.RegisterScoped<IDomainEventPublisherInternal, DomainEventPublisherInternal>();
+        dependencyInjectionContainer.RegisterScoped<IDomainEventPublisher, DomainEventPublisher>(dependencyInjectionContainer =>
+        {
+            return new DomainEventPublisher(dependencyInjectionContainer.Resolve<IDomainEventPublisherInternal>());
+        });
+        dependencyInjectionContainer.RegisterScoped<IDomainEventSubscriber, DomainEventSubscriber>();
+    }
+}
---
+++ src/MCB.Core.Domain/DomainEvents/DomainEventPublisher.cs
@@ -0,0 +1,24 @@
+﻿using MCB.Core.Domain.Abstractions.DomainEvents;
+using MCB.Core.Domain.DomainEvents.Interfaces;
+using MCB.Core.Domain.Entities.Abstractions.DomainEvents;
+
+namespace MCB.Core.Domain.DomainEvents;
+
+internal class DomainEventPublisher
+    : IDomainEventPublisher
+{
+    // Fields
+    private readonly IDomainEventPublisherInternal _DomainEventPublisherInternal;
+
+    // Constructors
+    internal DomainEventPublisher(IDomainEventPublisherInternal DomainEventPublisherInternal)
+    {
+        _DomainEventPublisherInternal = DomainEventPublisherInternal;
+    }
+
+    // Public Methods
+    public Task PublishDomainEventAsync(IDomainEvent DomainEvent, CancellationToken cancellationToken)
+    {
+        return _DomainEventPublisherInternal.PublishAsync(DomainEvent, cancellationToken);
+    }
+}
